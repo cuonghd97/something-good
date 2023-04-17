210de41f79a9b4a6a0a5cee6968f1ad803356304
Cuong <cuong.hoangduc>
2023-04-17 08:23:24 +0000

    factory method pattern

---
+++ factorypattern/src/main/java/dev/thesemicolon/App.java
@@ -0,0 +1,11 @@
+package dev.thesemicolon;
+
+/**
+ * Hello world!
+ * This is factory method pattern
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
